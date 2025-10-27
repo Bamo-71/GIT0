64a9dc3ca6725f87192b42a1c0c044bbecfd2c3a
Bamo-71 <bamorydebarnoothniel@gmail.com>
2025-10-27 13:51:23 +0000

    Add files via upload

---
+++ U1.R
@@ -0,0 +1,145 @@
+names(vec)<-c("A","B","C","D")
+vec
+names(vec)<-c(a,"D")
+names(vec)<-c(a,"E")
+vec
+vec[3]
+vec["E"]
+b<-list("a", 1L, 1.5, TRUE)
+b
+list(a, 1L, 1.5, TRUE)
+list(list(list(1 , 3, 5)))
+str(list("a", 1L, 1.5, TRUE))
+z
+z<-list(list(list(1 , 3, 5)))#liste engrener avec d'autr liste
+z
+str(z)
+bn<-list("caractere"=1,"Entier"=1L,"decimal"=1.5,"bool??en"=TRUE)
+bn
+bn<-list("caractere"=a,"Entier"=1L,"decimal"=1.5,"bool??en"=TRUE)
+bn
+#
+#
+install.packages("tidyverse")
+library(lubridate)
+today()
+now()
+d=now()
+d=now()
+typeof(d)
+typeof(d)
+d1=today()
+typeof(d1)
+ymd(today())
+d2=ymd(today())
+d2
+mdy(d2)
+mdy(d2)
+dmy("05-12-2021")
+as_date(now())
+#
+#
+z<-data.frame(x=c(17,13,18,12),Y=c(15,15,12,14))
+z
+z[2,2]
+z[2,3]
+z[2,1]
+file.create("fichher1.csv")
+file.copy("fichher1",C:\Users\bamor\Downloads)
+file.copy("fichher1","C:/Users/bamor/Downloads")
+file.copy("fichher1","C:/Users/bamor/Downloads")
+file.copy("fichher1","Ma_musique")
+file.copy("fichher1","Red_Alert_3_Uprising")
+file.copy("fichher1.csv","Balabolka")
+unlink("usedcars1")
+unlink("usedcars_1.csv")
+matrix(c(1:7))
+matrix(c(1:7),ncol = 3)
+matrix(c(1:7),ncol = 2)
+matrix(c(1:9),nrow = 2)
+#
+#
+data("airquality")
+force(airquality)
+force(airquality)
+View("airquality")
+airquality["Solar.R"]>150 & airquality["Wind"]>10
+airquality["Solar.R"]>150 AND airquality["Wind"]>10
+airquality["Solar.R"]>150 | airquality["Wind"]>10#OU
+airquality["Solar.R"]!=150 | airquality["day"]!=1
+airquality["Solar.R"]!=150 | airquality["Day"]!=1
+if(x<2){
+  if(x<2){
+    print("verit??")
+  }
+  if(airquality["Solar.R"]<150){
+    print("journ??e non ensoleir")
+  }
+  if(airquality[1,"Solar.R"]<150){
+  }
+  if(airquality["Solar.R"]<150){
+    print("journ??e non ensoleir")
+    print("journ??e non ensoleir")
+  }
+  f(airquality[1,"Solar.R"]<150){
+  }
+  if(airquality[1,"Solar.R"]<150){
+    if(airquality[1,"Solar.R"]<150){
+      print("journ??e non ensoleir")
+      print("il y a soleil m??me")
+    }
+    if(airquality$Ozone[1]=41){
+      if(airquality$Ozone[1]=41){
+      }else{
+        print("dommage")
+      }
+      if(airquality$Ozone[1]<41){
+        if(airquality$Ozone[1]<41){
+          print("BAMO a trouver")
+          print("dommage")
+          print("dommage")
+        }
+        if(airquality$Ozone[1]=41){
+          print("BAMO a trouver")
+          print("dommage")
+        }
+        if(airquality$Ozone[1]<-41){
+          if(airquality$Ozone[1]<-41){
+            print("BAMO a trouver")
+          }else if(airquality$Ozone[1]<-412){
+          }else{
+            print("aa")
+          }
+        }
+        if(airquality$Ozone[1]==41){
+          print("BAMO a trouver")
+        }else if(airquality$Ozone[1]<-412){
+          print("dommage")
+        }
+        if(airquality$Ozone[1]<-42){
+          print("BAMO a trouver")
+          print("BAMO a trouver")
+          print("dommage")
+          print("dommage")
+        }else{
+          print("aa")
+          if(airquality$Ozone[1]<-42){
+          }else if(airquality$Ozone[1]<-412){
+          }else{
+            print("aa")
+            if(airquality$Ozone[1]<-42){
+            }else if(airquality$Ozone[1]<-41){
+              print("dommage")
+              print("aa")
+            }
+            ###quand on utilise la fonction "else if" on utilise "==" au lieu de "<-"
+            ###quand on utilise la fonction "else if" on utilise "==" au lieu de "<-"
+            install.packages()
+            ###quand on utilise la fonction "else if" on utilise "==" au lieu de "<-"
+            install.packages("tidyverse")
+            install.packages("tidyverse")
+            nstall.packages("tidyverse")
+            read.csv(file = "usedcars.csv")
+##MODULE 3
+summarise(body_)
+  pen          
